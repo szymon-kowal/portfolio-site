7bcf089f04bd8ff957c3d7fb340b8ee30d2c1c3e
szymek <00szymon.kowal@gmail.com>
2023-11-15 13:57:00 +0100

    Commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+const btnGit = document.querySelector('.git');
+
+
+function redirectToGitHub() {
+    const githubURL = 'https://github.com/szymon-kowal';
+    window.location.href = githubURL;
+}
+
+btnGit.addEventListener('click', redirectToGitHub);
